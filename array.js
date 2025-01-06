9f7ebe6718cf5634feab8c813f115f6d4141f8ac
Akthersumiya <mstsumiyaakther2004@gmail.com>
2025-01-06 01:19:15 -0800

    first commit

---
+++ array.js
@@ -0,0 +1,33 @@
+// let number = [1,2,3,4,5,6,7,8,9];
+// number.push(10,12)
+// console.log(number);
+
+// let number = [10,20,30,40]
+// number.pop()
+// console.log(number);
+
+// let number = [20,30,40]
+// let last = number.pop()
+// console.log(last);
+
+// let secondNumber = [20,30,40];
+// let first = secondNumber.shift();
+// console.log(first);
+
+
+
+// let numbers = [10,20,30,40,50];
+// let newNumbers = [];
+// newNumbers.push(numbers.pop())
+// newNumbers.push(numbers.pop())
+
+// console.log(numbers);
+// console.log(newNumbers);
+
+// let arr1 = ["lilu", "milu", "lalu"];
+// let arr2 = ["hablu", "moblu", "lavlu"];
+
+// let combined = arr1.concat(arr2);
+
+// console.log(combined);
+
---
+++ loop.js
@@ -0,0 +1,128 @@
+// 1, For loop
+// 2, While loop 
+// 3, Do...While
+// 4, for... in
+// 5, for...of
+
+
+
+// for(Infinity; Condition; increment/decrement)
+    // console.log(Object)
+
+
+
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+
+// let i = 0;
+// while(i <= 5){
+//     console.log(i);
+//     i++
+// }
+
+// do{
+
+// }while(condition)
+
+
+// let i = 1;
+
+// do{
+//     console.log(i);
+//     i++
+// }while(i <= 5);
+
+
+
+// let i = 1;
+// while(i <= 20){
+//     if(i % 2 === 0){
+
+//         console.log(i);
+
+//     }
+//    i++
+// } 
+
+
+// let i = 1;
+
+// do{
+//     console.log("sumiya");
+// console.log(i)
+//     i ++
+// }while(i <= 5);
+
+
+// let fruits = ["apple", "banana", "orange", "lichi"];
+
+
+
+// for(let i = 0 ; i < fruits.length ; i++){
+//     console.log(fruits[i]);
+// }
+
+
+// let number = [5, 10, 15, 20, 25, 30 ];
+
+// let sum = 0;
+
+// for(let i = -- 0; i < number.length; i++ ){
+//     sum += number[i];
+
+// }console.log(sum);
+
+
+
+// let fruits = ["apple", "banana", "orange", "lichi"];
+
+// let i = 0;
+
+// while ( i < fruits.length){
+
+//     console.log(fruits[i]);
+
+//     i++;
+// }
+
+
+// let number = [1 ,2 ,3 ,4 ,5];
+// let text = [];
+// for(let i = 0; i < number.length; i++){
+//     text += number[i]
+//     console.log(text);
+// }
+
+
+// let number = [1, 2,3,4,5];
+// number.reverse()
+// console.log(number);
+
+// let  numbers = [ 12, 34,13,25,78 ];
+// let max = numbers[0];
+// let min = numbers[0];
+
+
+// for (let i = 0; i < numbers.length; i++){
+//     if (numbers[i] > max){
+//         max = numbers[i];
+//     }
+//     if (numbers[i] <min){
+//         min = numbers[i];
+//     }
+// }
+
+// console.log("Max number:", max);
+// console.log("Min number:", min);
+    
+
+const number = [1,2,3,4,5,6,7];
+let reverse = [];
+for(let i = number.length-1; i>=0; i--){
+    reverse.push(number[i])
+}
+
+console.log(reverse);
